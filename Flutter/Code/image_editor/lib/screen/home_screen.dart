881cac97e8bf6d1e8c270e5205aef3f6782ec378
Hyun-w <smy335264@gmail.com>
2023-06-25 15:48:16 +0900

    image_editor

--- Flutter/Code/image_editor/lib/screen/home_screen.dart
+++ Flutter/Code/image_editor/lib/screen/home_screen.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:image_editor/component/emotion_sticker.dart';
 import 'package:image_editor/component/footer.dart';
 import 'package:image_editor/component/main_app_bar.dart';
+import 'package:image_editor/model/sticker_model.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:uuid/uuid.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/services.dart';
+import 'dart:typed_data';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 
 class HomeScreen extends StatefulWidget{
   const HomeScreen({Key?key}):super(key: key);
@@ -12,6 +20,9 @@ class HomeScreen extends StatefulWidget{
 
 class _HomeScreenState extends State<HomeScreen> {
   XFile? image;
+  Set<StickerModel> stickers= {};
+  String? selectedId;
+  GlobalKey imgKey = GlobalKey();
 
   @override
   Widget build(BuildContext context){
@@ -51,15 +62,46 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void onSaveImage(){} 
+  void onSaveImage() async {
+    RenderRepaintBoundary boundary = imgKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage();
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    Uint8List pngBytes = byteData!.buffer.asUint8List();
+
+    await ImageGallerySaver.saveImage(pngBytes, quality: 100);
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('저장되었습니다!'),)
+    );
+  } 
 
   Widget renderBody(){
     if(image!=null){
-      return Positioned.fill(
-        child: InteractiveViewer(
-          child: Image.file(
-            File(image!.path),
-            fit: BoxFit.cover,
+      return RepaintBoundary(
+        key: imgKey,
+        child: Positioned.fill(
+          child: InteractiveViewer(          
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Image.file(
+                  File(image!.path),
+                  fit: BoxFit.cover,
+                ),
+                ...stickers.map(
+                  (sticker) => Center(
+                    child: EmotionSticker(
+                      key: ObjectKey(sticker.id),
+                      onTransform: (){
+                        onTransform(sticker.id);
+                      },
+                      imgPath: sticker.imgPath,
+                      isSelected: selectedId == sticker.id,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       );
@@ -76,5 +118,23 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  void onEmotionTap(int index){}
+  void onEmotionTap(int index)async{
+    setState(() {
+      stickers = {
+        ...stickers,
+        StickerModel(
+          id: Uuid().v4(), 
+          imgPath: 'asset/img/emotion_$index.png',
+        ),
+      };
+    });
+  }
+  void onTransform(String id){
+    selectedId=id;
+  }
+  void onDeleteItem() async {
+    setState(() {
+      stickers = stickers.where((sticker)=>sticker.id != selectedId).toSet();
+    });
+  }
 }
