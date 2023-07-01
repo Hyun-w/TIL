83b391d869564917ebb4732e3d79b482f3a740b3
Hyun-w <smy335264@gmail.com>
2023-07-01 23:52:43 +0900

    calendar

--- Flutter/Code/calendar_schedular/lib/screen/home_screen.dart
+++ Flutter/Code/calendar_schedular/lib/screen/home_screen.dart
@@ -1,12 +1,39 @@
+import "package:calendar_schedular/component/main_calendar.dart";
 import "package:flutter/material.dart";
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key?key}):super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}):super(key: key);
+
+  @override
+  State<HomeScreen> createState()=>_HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Text('h'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            MainCalendar(
+              selectedDate: selectedDate,
+              onDaySelected: onDaySelected,
+            ),
+          ],
+        )
+      ),
     );
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate){
+    setState(() {
+      this.selectedDate = selectedDate;      
+    });
   }
 }
